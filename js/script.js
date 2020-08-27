fc1efbfd967caaf39d0adf3749825d37699e8507
Pedro Franco <contatopedro@outlook.com>
2020-08-27 19:31:50 -0300

    morecoins

--- js/script.js
+++ js/script.js
@@ -4,14 +4,27 @@ function convertCurrency(){
     var fromvalue = document.getElementById("valor1")
     var tovalue = document.getElementById("valor2")
     var url = ("https://api.exchangeratesapi.io/latest?symbols=" + from + "," + to); //manipulando o link da API
-    fetch(url) //mecanismo para puxar api
-    .then(res => res.json()) //chamar o JSON
-    .then(date => {
-        var oneUnit = (date.rates[to] / date.rates[from]); //date é a parada da api, o rates está dentro do link, e dentro do rates eu puxo o que eu quiser
-        var amt = fromvalue.value;
-        var res = (oneUnit*amt);
-        tovalue.value = res.toFixed(3); //para deixar 3 casas decimais
-    })
+    var url2 = ("https://api.exchangeratesapi.io/latest?base=THB")
+    if (from == 'EUR' || to == 'EUR'){
+      fetch(url2) //mecanismo para puxar api
+      .then(res => res.json()) //chamar o JSON
+      .then(date => {
+          var oneUnit = (date.rates[to] / date.rates[from]); //date é a parada da api, o rates está dentro do link, e dentro do rates eu puxo o que eu quiser
+          var amt = fromvalue.value;
+          var res = (oneUnit*amt);
+          tovalue.value = res.toFixed(3); //para deixar 3 casas decimais
+      })
+    } else{
+        fetch(url) //mecanismo para puxar api
+            .then(res => res.json()) //chamar o JSON
+            .then(date => {
+                var oneUnit = (date.rates[to] / date.rates[from]); //date é a parada da api, o rates está dentro do link, e dentro do rates eu puxo o que eu quiser
+                var amt = fromvalue.value;
+                var res = (oneUnit*amt);
+                tovalue.value = res.toFixed(3); //para deixar 3 casas decimais
+            })
+    }
+    
 }
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
