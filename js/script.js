5680f4476b45d6ded2a3dc9c804916275655f484
Pedro Franco <contatopedro@outlook.com>
2020-08-27 16:43:53 -0300

    few changes

--- js/script.js
+++ js/script.js
@@ -13,3 +13,13 @@ function convertCurrency(){
         tovalue.value = res.toFixed(3); //para deixar 3 casas decimais
     })
 }
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
